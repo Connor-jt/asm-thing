6310a104fb66b87b484e2da7ce2d2c1bdd464fb5
Connor J Thompson <ct770a@gmail.com>
2024-11-26 13:40:15 +1100

    improved scene navigation to now only move when middle mouse button is being held down

--- SRC/Camera.asm
+++ SRC/Camera.asm
@@ -3,6 +3,7 @@ public dCameraX
 public dCameraY
 
 extern dKeyMap : byte
+extern dHeldKeyMap : byte
 extern dMouseX : dword
 extern dMouseY : dword
 
@@ -14,18 +15,9 @@ dCameraY dword 0
 dPrevMouseX dword 0
 dPrevMouseY dword 0
 
-dIsMoving dword 0
-
 .code
 
 CameraTick PROC
-	; toggle movement state if middle mouse was pressed
-		lea rcx, dKeyMap
-		mov al, byte ptr [rcx+4]
-		cmp al, 0
-		je block10
-			xor dIsMoving, 1
-		block10:
 	; calc X offset since last move
 		mov ecx, dMouseX
 		sub ecx, dPrevMouseX
@@ -37,7 +29,9 @@ CameraTick PROC
 		mov eax, dMouseY
 		mov dPrevMouseY, eax
 	; if camera move enabled, apply mouse movement
-		cmp dIsMoving, 0
+		lea rax, dHeldKeyMap
+		mov al, byte ptr [rax+4]
+		cmp al, 0
 		je block11
 			sub dCameraX, ecx
 			sub dCameraY, edx
