2b6a6d5c70cbea718bfdee0612a5d5e5be0cf16e
Connor J Thompson <ct770a@gmail.com>
2024-12-04 01:18:14 +1100

    added in the code to convert x,y coord to grid/section/quad indices

--- SRC/grid/MapGrid.asm
+++ SRC/grid/MapGrid.asm
@@ -24,8 +24,40 @@ Grid qword 4096 dup(0) ; 32kb !!
 ; dx: Y
 ; cx: X
 GridCheckTile PROC
-	; transform into GRID layer	
-	
+	; r8b: quad array index
+	; r9b: section array index
+	; cx: grid array index
+
+	; transform coords, so coord 0,0 becomes the center most tile
+	; todo: make 0,0 not the perfect center so we dont have to constantly realloc blocks
+		add cx, 8000h
+		add dx, 8000h
+	; get quadrant layer coords (indexes the tile)
+		mov r8b, cx
+		mov ax,  dx
+		and r8b, 31
+		and ax,  31
+		; combine Y into X
+		shl  ax, 5
+		or  r8b, ax 
+	; get section layer coords (indexes the quadrant)
+		sar cx, 5
+		sar dx, 5
+		mov r9b, cx
+		mov ax,  dx
+		and r9b, 31
+		and ax,  31
+		; combine Y into X
+		shl ax,  5
+		or  r9b, ax 
+	; get grid layer coords
+		sar cx, 5
+		sar dx, 5
+		; combine Y into X
+		shl dx, 6
+		or  cx, dx 
+		
+		
 
 
 GridCheckTile ENDP
